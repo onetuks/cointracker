69297437d51dd1b7675d12e98daaa3b50c7d64a8
SY97P <onetuks@naver.com>
2023-03-20 22:06:38 +0900

    #5.7 Nested Routes part One

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from './App';
+import Chart from "./routes/Chart";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
+import Price from "./routes/Price";
 
 const router = createBrowserRouter([
     {
@@ -13,8 +15,20 @@ const router = createBrowserRouter([
                 element: <Coins />
             },
             {
-                path: ":coinId",
+                path: ":coinId/*",
                 element: <Coin />,
+                // React-Router-Dom@6.x 
+                // Nested Routes 사용 방법
+                children: [
+                    {
+                        path: "price",
+                        element: <Price />
+                    },
+                    {
+                        path: "chart",
+                        element: <Chart />
+                    }
+                ],
             }
         ],
     }
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -25,6 +25,31 @@ const Loader = styled.span`
   display: block;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    span: first-child { 
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+
+const Description = styled.p`
+    margin: 20px 0px;
+`;
+
 interface RouteState {
   state: {
     name: string;
@@ -34,78 +59,78 @@ interface RouteState {
 
 // Array interface 선언 방법
 interface IData {
-    id: string;
-    name: string;
-    position: string;
+  id: string;
+  name: string;
+  position: string;
 }
 
 interface IInfoData {
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    is_new: boolean;
-    is_active: boolean;
-    type: string;
-    logo: string;
-    tags: IData[];
-    team: IData[];
-    description: string;
-    message: string;
-    open_source: boolean;
-    started_at: string;
-    development_status: string;
-    hardware_wallet: boolean;
-    proof_type: string;
-    org_structure: string;
-    hash_algorithm: string;
-    links: object;
-    links_extended: object;
-    whitepaper: object;
-    last_data_at: string;
-    first_data_at: string;
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  tags: IData[];
+  team: IData[];
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  last_data_at: string;
+  first_data_at: string;
 }
 
 // object interface 선언방법
-interface IPriceData { 
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    circulating_supply: number;
-    total_supply: number;
-    max_supply: number;
-    beta_value: number;
-    first_data_at: string;
-    last_updated: string;
-    quotes: {
-        USD: {
-            ath_date : string;
-            ath_price : number;
-            market_cap : number;
-            market_cap_change_24h : number;
-            percent_change_1h : number;
-            percent_change_1y : number;
-            percent_change_6h : number;
-            percent_change_7d : number;
-            percent_change_12h : number;
-            percent_change_15m : number;
-            percent_change_24h : number;
-            percent_change_30d : number;
-            percent_change_30m : number;
-            percent_from_price_ath : number;
-            price : number;
-            volume_24h : number;
-            volume_24h_change_24h : number;
-        }
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
     };
+  };
 }
 
 function Coin() {
   const { coinId } = useParams();
   const { state } = useLocation() as RouteState;
   const [loading, setLoading] = useState(true);
-  // state은 Generic으로 선언해야함. 
+  // state은 Generic으로 선언해야함.
   const [info, setInfo] = useState<IInfoData>();
   const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
@@ -116,26 +141,58 @@ function Coin() {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
-      
+
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      
+
       console.log(infoData);
       console.log(priceData);
-      
+
       setInfo(infoData);
       setPriceInfo(priceData);
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        {/* 이제 url을 복사해서 직접 coinId로 들어가도 보임 */}
+        <Title>{ state?.name ? state.name : loading ? "Loading..." : info?.name }</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : priceInfo?.quotes.USD.ath_date}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Outlet />
+        </>
+      )}
     </Container>
   );
 }
